4b4128260c0cd19cbee6bc9d8fd79f1b101f4e2a
jmcshane <james.mcshane@superorbital.io>
2021-12-02 07:45:54 -0500

    Day 2 part 2

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use std::path::Path;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-    let mut position = Movement{x:0,y:0};
+    let mut position = Movement{x:0,y:0,aim:0};
     if let Ok(lines) = read_lines(filename) {
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
@@ -22,27 +22,24 @@ fn main() {
 struct Movement {
     x: i32,
     y: i32,
+    aim: i32,
 }
 
 impl Summable for Movement {
     fn zero() -> Self {
-        return Movement{x: 0, y:0}
+        return Movement{x: 0, y:0, aim: 0}
     }
     fn add_assign_ref(&mut self, rhs: &Self) {
-        self.x.add_assign_ref(&rhs.x);
-        self.y.add_assign_ref(&rhs.y);
     }
     fn sub_assign_ref(&mut self, rhs: &Self) {
-        self.x.sub_assign_ref(&rhs.x);
-        self.y.sub_assign_ref(&rhs.y);
     }
+    //only use add_assign for now
     fn add_assign(&mut self, rhs: Self) {
         self.x.add_assign(rhs.x);
-        self.y.add_assign(rhs.y);
+        self.y.add_assign(rhs.x * self.aim);
+        self.aim.add_assign(rhs.aim);
     }
     fn sub_assign(&mut self, rhs: Self) {
-        self.x.sub_assign(rhs.x);
-        self.y.sub_assign(rhs.y);
     }
 }
 
@@ -50,12 +47,12 @@ fn apply_direction(input: String) -> Movement {
     let split : Vec<&str> = input.split(" ").collect();
     let distance = split[1].parse::<i32>().unwrap();
     match (*split[0]).to_lowercase().as_str() {
-        "up" => return Movement{x:0, y: -1 * distance},
-        "down" => return Movement{x: 0, y: distance},
-        "forward" => return Movement{x: distance, y: 0},
+        "up" => return Movement{x:0, y:0, aim: -1 * distance},
+        "down" => return Movement{x: 0, y:0, aim: distance},
+        "forward" => return Movement{x: distance, y: 0, aim: 0},
         _ => println!("Invalid instruction"),
     }
-    return Movement{x:0,y:0};
+    return Movement{x:0,y:0, aim:0};
 }
 
 // The output is wrapped in a Result to allow matching on errors
